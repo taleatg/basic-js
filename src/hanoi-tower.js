0e6f992706e4b2e859880b3dec0293fd1d4b0c76
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-10-18 21:57:23 +0400

    Tower of Hanoi completed

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+  //throw new CustomError('Not implemented');
+  let turns;
+  let key1 = Math.pow(2,disksNumber)-1;
+  let obj = {
+    turns: key1,
+    seconds: Math.floor((3600*key1)/turnsSpeed),
+  }
+  return obj;
 };
