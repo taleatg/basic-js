ee922d6395fe49feecf2f070f66320080f13250d
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-10-28 23:51:47 +0400

    dream-team start

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let team = '';
+  if (Array.isArray(members)) {
+  for (let i = 0; i < members.length; i++) {
+    
+      let str = members[i].split('');
+        let name = str[0].toUpperCase();
+        team += name;
+      };
+      
+      return team.split('').sort().join('');
+    } else {
+      return false;
+    }
+   
+
 };
