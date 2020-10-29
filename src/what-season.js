d7a51fa40698496e2a6b41e6f7f1f75f92582d8a
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-10-29 23:40:57 +0400

    initial commit

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const summary = [winter, spring, summer, autumn, fall];
+
+  if ( date == null) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!date || (date = 0)) {
+    return 'Error';
+  } else {
+
+  let month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+  }
+ }
 };
