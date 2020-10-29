1e52f3f6f2ec74b7c0627d6e850d7cd457162835
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-10-29 22:19:27 +0400

    task "Dream Team" completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,22 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   let team = '';
-  if (Array.isArray(members)) {
-  for (let i = 0; i < members.length; i++) {
-    
-      let str = members[i].split('');
-        let name = str[0].toUpperCase();
-        team += name;
-      };
-      
-      return team.split('').sort().join('');
-    } else {
-      return false;
-    }
-   
+  if (Array.isArray(members)) { 
 
-};
+      for (let i = 0; i < members.length; i++) {
+        
+        if (typeof members[i] == 'string') {
+
+          let str = members[i].trim().split('');
+            let name = str[0].toUpperCase();
+            team += name;
+          
+        }
+      };
+        
+  return team.split('').sort().join('');
+  
+  } else {
+        return false;
+      }
+  };
