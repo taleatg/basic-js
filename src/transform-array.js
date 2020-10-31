fe6bf2a693b04484fd1fbee3d286ccee81c2f149
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-10-31 23:58:14 +0400

    start task 'transform-array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+let array = arr;
+
+if (!Array.isArray(arr)) { 
+    throw new Error('THROWN');
+}
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i] === '--discard-next') {
+        if (i !== array.length - 1) {
+        array = array.splice(i, 2);
+        }
+    } else if (array[i] === '--discard-prev') {
+        if (i !== 0) {
+        array = array.splice(i - 1, 2);
+        }
+    } else if (array[i] === '--double-next') {
+        if (i !== array.length - 1) {
+        array[i] = array[i + 1];
+        
+        }
+    } else if (array[i] === '--double-prev') {
+        if (i !== 0) {
+        array[i] = array[i - 1];
+        }
+    }
+  }
+  return array;
+
 };
