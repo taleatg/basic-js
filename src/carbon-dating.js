282a337ef57f3624dabf7566b8505b743e31c44d
Tatsiana Dashuk <t_korbyt@mail.ru>
2020-11-01 12:04:47 +0400

    task 'carbon dating' completed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  
+  let age = Number; 
+    if ((typeof sampleActivity === 'String') && (sampleActivity < MODERN_ACTIVITY)){
+      age = Math.ceil(Math.log2(MODERN_ACTIVITY/sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+      return age;
+    } else {
+      return false;
+    }
   
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -2,6 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
 let array = arr;
+let delArra = [];
 
 if (!Array.isArray(arr)) { 
     throw new Error('THROWN');
@@ -10,21 +11,28 @@ if (!Array.isArray(arr)) {
 
     if (array[i] === '--discard-next') {
         if (i !== array.length - 1) {
-        array = array.splice(i, 2);
-        }
+            delArr = array.splice(i, 2);
+        } else {
+            delArr = array.pop();
+          }
     } else if (array[i] === '--discard-prev') {
         if (i !== 0) {
-        array = array.splice(i - 1, 2);
-        }
+            delArr = array.splice(i - 1, 2);
+        } else {
+            delArr = array.shift();
+          }
     } else if (array[i] === '--double-next') {
         if (i !== array.length - 1) {
-        array[i] = array[i + 1];
-        
-        }
+            delArr = array.splice(i, 2);
+        } else {
+            delArr = array.pop();
+          }
     } else if (array[i] === '--double-prev') {
         if (i !== 0) {
-        array[i] = array[i - 1];
-        }
+            delArr = array.splice(i, 1);
+        } else {
+            delArr = array.shift();
+          }
     }
   }
   return array;
